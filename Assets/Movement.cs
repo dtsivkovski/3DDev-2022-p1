ed55e421ceca9dc072b32b090e440cdd6d5f0eae
dtsivkovski <dtsivkovs@gmail.com>
2022-02-12 13:26:32 -0800

    first commit

---
+++ Assets/Movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float forwardForce = 1500f;
+    public float sidewaysForce = 500f;
+
+    // FixedUpdate makes physics more smooth
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d")) {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a")) {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
