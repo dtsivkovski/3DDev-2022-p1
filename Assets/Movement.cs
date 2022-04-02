5b19a664941e64a55d26486165421f191ff8766d
dtsivkovski <dtsivkovs@gmail.com>
2022-04-01 19:54:18 -0700

    meeting 4/1/22

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -15,10 +15,10 @@ public class Movement : MonoBehaviour
         rb.AddForce(0, 0, forwardForce * Time.deltaTime);
 
         if (Input.GetKey("d")) {
-            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
         if (Input.GetKey("a")) {
-            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
     }
 }
